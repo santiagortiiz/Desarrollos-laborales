49f5e851e61836911da8c3562cd1b4adba2db89d
Santiago Ortiz Ceballos <63321079+santiagortiiz@users.noreply.github.com>
2021-03-23 19:12:28 -0500

    Recepción de parámetros enviados de NodeJS

--- Algoritmos/Procesamiento BigData/Flujo de Procesamiento/main.py
+++ Algoritmos/Procesamiento BigData/Flujo de Procesamiento/main.py
@@ -5,17 +5,26 @@ Created on Wed Mar 17 09:47:12 2021
 @author: SANTIAGO
 """
 
+import sys
 from Components.Processor import Processor
 
 if (__name__ == "__main__"):            
     # Beware with the slash, path must be with "/" or "\\"
-    source_folder = "D:/Laboral/Vitalbox/Proyectos/Clientes/Katia/Procesamiento Carga Masiva/Archivos Fuent/" 
-    summary_file = "ResumenProcesamiento.txt"
-    delimiter = '|'                 # Delimiter of the files to be processed
-    threshold = 1                   # Amount of files needed to run the process
-    table_target = "enroladoss"     # Name of the table in which the data will be storage
-    keyword = "RECLAMACION"         # Key word to search the files in the source folder
-    url = None                      # URL of the service to consume
+    # source_folder = "D:/Laboral/Vitalbox/Proyectos/Clientes/Katia/Procesamiento Carga Masiva/Archivos Fuent/" 
+    # summary_file = "ResumenProcesamiento.txt"
+    # delimiter = '|'                 # Delimiter of the files to be processed
+    # threshold = 0                   # Amount of files needed to run the process (number of files >= threshold)
+    # table_target = "enroladoss"     # Name of the table in which the data will be storage
+    # keyword = "RECLAMACION"         # Key word to search the files in the source folder
+    # url = None                      # URL of the service to consume
+   
+    source_folder = sys.argv[1]
+    summary_file = sys.argv[2]
+    threshold = sys.argv[3]         # Amount of files needed to run the process (number of files >= threshold)
+    keyword = sys.argv[4]           # Key word to search the files in the source folder
+    delimiter = sys.argv[5]         # Delimiter of the files to be processed
+    table_target = sys.argv[6]      # Name of the table in which the data will be storage
+    url = sys.argv[7]               # URL of the service to consume
     
     processor = Processor(source_folder=source_folder, summary_file=summary_file, 
                           keyword=keyword, delimiter=delimiter, 
